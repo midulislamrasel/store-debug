015af9b9b912ad2612a358fb195eeec3ffc10fdd
Midul-islam-Russell <midulraddy121@gmail.com>
2022-10-26 00:25:03 +0600

    card add

--- js/app.js
+++ js/app.js
@@ -42,10 +42,9 @@ const showProducts = (products) => {
 };
 
 let count = 0;
-
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
